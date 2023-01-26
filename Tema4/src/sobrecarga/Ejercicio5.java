ae608cbb629b1e43f6aeaec1ea78b6bccc536c39
igarcia <igarcar@iesnervion.es>
2023-01-26 12:39:38 +0100

    26/01/2023

--- Tema4/src/sobrecarga/Ejercicio5.java
+++ Tema4/src/sobrecarga/Ejercicio5.java
@@ -2,35 +2,70 @@ package sobrecarga;
 
 import java.util.Scanner;
 
+/**
+ * En esta clase pediremos al usuario la cantidad de numeros aleatorios que
+ * quiere generar y su rango y lo generaremos.
+ * 
+ * @author igarcia
+ *
+ */
+
 public class Ejercicio5 {
 
+	/**
+	 * En esta funcion generaremos una cierta cantidad de numeros aleatorios que
+	 * recibiremos del usuario.
+	 * 
+	 * @param a Cantidad de numeros aleatorios a generar
+	 */
 	static void numerosAleatorios(int a) {
 
 		// Generamos a numeros aleatorios
 		for (int i = 0; i < a; i++) {
-			System.out.printf("%.2f\n",(double) (Math.random()));
+			System.out.printf("%.2f\n", (double) (Math.random()));
 		}
 
 	}
 
+	/**
+	 * En esta funcion generaremos una cierta cantidad de numeros aleatorios con un
+	 * numero maximo enviados por el usuario
+	 * 
+	 * @param a Cantidad de numeros aleatorios a generar
+	 * @param b Numero maximo del numero aleatorio
+	 */
 	static void numerosAleatorios(int a, int b) {
 
 		// Generamos a numeros aleatorios entre 0 y b
 		for (int i = 0; i < a; i++) {
-			System.out.println((int) (Math.random() * b)+1);
+			System.out.println((int) (Math.random() * b) + 1);
 		}
 
 	}
 
+	/**
+	 * En esta clase generaremos una cierta cantidad de numeros aleatorios con un
+	 * rango pedido por el usuario
+	 * 
+	 * @param a Cantidad de numeros aleatorios a generar
+	 * @param b Numero maximo de los numeros aleatorios
+	 * @param c Numero minimo de los numeros aleatorios
+	 */
 	static void numerosAleatorios(int a, int b, int c) {
 
 		// Generamos a numeros aleatorios entre b y c
 		for (int i = 0; i < a; i++) {
-			System.out.println((int) (Math.random()*(b-c+1)+c));
+			System.out.println((int) (Math.random() * (b - c + 1) + c));
 		}
 
 	}
 
+	/**
+	 * En la clase main pediremos los parametros al usuario y llamaremos a las
+	 * correspondientes clases
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		// Declaración de variables
